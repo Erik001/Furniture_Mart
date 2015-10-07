374212ebc6236895e0601eb63be801ddd43a72a2
Erik Torres <erik.torres2004@gmail.com>
2015-10-06 21:48:30 -0500

    Table creation from an array of objects added

--- app/src/main/java/com/treesoftware/furnituremart/OrderStatus.java
+++ app/src/main/java/com/treesoftware/furnituremart/OrderStatus.java
@@ -1,12 +1,13 @@
 package com.treesoftware.furnituremart;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.treesoftware.furnituremart.data.Order;
 
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 
 public class OrderStatus extends AppCompatActivity {
 
+    LinearLayout layout;
     private ArrayList<Order> orderList = new ArrayList<Order>();
 
     @Override
@@ -23,7 +25,39 @@ public class OrderStatus extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        layout = (LinearLayout) findViewById(R.id.table_container);
+
         orderList = OrderDetail.orderList;
+
+        TableLayout tl = new TableLayout(this);
+
+        int colorCounter = 0;
+        for (Order order : orderList) {
+            TableRow tr = new TableRow(this);
+
+            TextView orderId = new TextView(this);
+            orderId.setText(order.getOrderId());
+            orderId.setPadding(60, 60, 60, 60);
+            tr.addView(orderId);
+
+            TextView orderDate = new TextView(this);
+            orderDate.setText(order.getOrderDate());
+            orderDate.setPadding(60, 60, 60, 60);
+            tr.addView(orderDate);
+
+            if (colorCounter % 2 == 0)
+                tr.setBackgroundColor(Color.GRAY);
+            else
+                tr.setBackgroundColor(Color.LTGRAY);
+
+            tl.addView(tr);
+
+            ++colorCounter;
+        }
+
+        layout.addView(tl);
+
+
 
     }
 
